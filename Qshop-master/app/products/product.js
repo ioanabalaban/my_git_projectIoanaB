9c373594ff16db181c39f9854998c043ee03c681
intern2016 <balaban.ioana04@gmail.com>
2016-07-08 12:51:21 +0300

    Product Button JavaScript

--- Qshop-master/app/products/product.js
+++ Qshop-master/app/products/product.js
@@ -57,3 +57,28 @@ document.addEventListener('DOMContentLoaded',function(){
 
 
 })
+
+function clickCounterPlus() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = localStorage.clickcount;
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+function clickCounterMinus() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)-1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = localStorage.clickcount;
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
