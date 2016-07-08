28967e73b8fe319b76b8ff6ee484a0f62a3d9999
intern2016 <balaban.ioana04@gmail.com>
2016-07-08 11:59:42 +0300

    Product JavaScript

--- Qshop-master/app/js/utils.js
+++ Qshop-master/app/js/utils.js
@@ -32,6 +32,7 @@ function hideError(inputId) {
 }
 
 
+
 /**
  * isEmptyInput - Helper to determine if an input is empty
  *
@@ -41,6 +42,38 @@ function hideError(inputId) {
 function isEmptyInput(inputId) {
     return !document.querySelector(inputId).value
 }
+function requireFields(form, inputs) {
+  //attach to each input an event listener so we can check for events
+  for(var i=0;i<inputs.length;i++) {
+    document.querySelector(inputs[i]).addEventListener("keyup",function() {
+      onchangeElem('#' + this.id)
+    })
+  }
+  //listen to the submit event
+  form.addEventListener('submit', function(event){
+      var hasError = false;
+      for(var i=0;i<inputs.length;i++) {
+        if(isEmptyInput(inputs[i])) {
+           hasError = true;
+           showError(inputs[i])
+        } else {
+          hideError(inputs[i])
+        }
+      }
+      //if at least one element had errors prevent default submit action
+      if(hasError) {
+        event.preventDefault();
+      }
+  })
+}
+
+/**
+ * findParentWithClass - Find the closest parent that has a the given className used
+ *
+ * @param  {DOMNode} node      The node to start the search form
+ * @param  {String} className The class we want to find being used
+ * @return {DOMNode|null}      Return the found node or null
+ */
 
 
 function findParentWithClass(node, className) {
@@ -55,39 +88,87 @@ function findParentWithClass(node, className) {
 }
 
 function getProductList() {
-    return [{
-        picture: '../assets/images/products/product-1.jpg',
-        name: 'Limited Price',
-        price: 299,
-        onSale: true
-    }, {
-        picture: '../assets/images/products/product-2.jpg',
-        name: 'Amazing Product',
-        price: 299,
-        onSale: false
-    }, {
-        picture: '../assets/images/products/product-3.jpg',
-        name: 'New Product',
-        price: 79,
-        onSale: false
-    } {
-        picture: '../assets/images/products/product-4.jpg',
-        name: 'Limited Product',
-        price: 99,
-        onSale: false
-    }]
+  return [{
+    picture: '../assets/images/products/product-1.jpg',
+    name: 'Limited Price',
+    price: 299,
+    onSale: true,
+    description: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis, massa fringilla consequat blandit, mauris ligula porta nisi, non tristique enim sapien vel nisl. Suspendisse vestibulum lobortis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent nec tempus nibh. Donec mollis commodo metus et fringilla. Etiam venenatis, diam id adipiscing convallis, nisi eros lobortis tellus, feugiat adipiscing ante ante sit amet dolor. Vestibulum vehicula scelerisque facilisis. Sed faucibus placerat bibendum. Maecenas sollicitudin commodo justo, quis hendrerit leo consequat ac. Proin sit amet risus sapien, eget interdum dui. Proin justo sapien, varius sit amet hendrerit id, egestas quis mauris.',
+    specifications: [{
+      label: 'prop 1',
+      value: 'value 1',
+    },{
+      label: 'prop 2',
+      value: 'value 2',
+    },{
+      label: 'prop 3',
+      value: 'value 3',
+    }],
+    reviews: [{
+      name: 'OLI 1',
+      text: 'Great product 6/7 with rice'
+    },{
+      name: 'OLI 2',
+      text: 'Great product over 9000 out 10'
+    }],
+    pictures: [
+      '../assets/images/products/product-1.jpg',
+      '../assets/images/products/product-2.jpg',
+      '../assets/images/products/product-3.jpg'
+    ]
+  },{
+    picture: '../assets/images/products/product-2.jpg',
+    name: 'Amazing Product',
+    price: 299,
+    onSale: false,
+    description: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis, massa fringilla consequat blandit, mauris ligula porta nisi, non tristique enim sapien vel nisl. Suspendisse vestibulum lobortis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent nec tempus nibh. Donec mollis commodo metus et fringilla. Etiam venenatis, diam id adipiscing convallis, nisi eros lobortis tellus, feugiat adipiscing ante ante sit amet dolor. Vestibulum vehicula scelerisque facilisis. Sed faucibus placerat bibendum. Maecenas sollicitudin commodo justo, quis hendrerit leo consequat ac. Proin sit amet risus sapien, eget interdum dui. Proin justo sapien, varius sit amet hendrerit id, egestas quis mauris.',
+    specifications: [{
+      label: 'prop 1 1',
+      value: 'value 1 1',
+    },{
+      label: 'prop 1 2',
+      value: 'value 1 2',
+    },{
+      label: 'prop 1 3',
+      value: 'value 1 3',
+    }],
+    reviews: [{
+      name: 'OLI 1',
+      text: 'Great product 6/7 with rice'
+    },{
+      name: 'OLI 2',
+      text: 'Great product over 9000 out 10'
+    }],
+    pictures: [
+      '../assets/images/products/product-1.jpg',
+      '../assets/images/products/product-2.jpg',
+      '../assets/images/products/product-3.jpg'
+    ]
+  },{
+    picture: '../assets/images/products/product-3.jpg',
+    name: 'New product',
+    price: 299,
+    onSale: false,
+    description: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis, massa fringilla consequat blandit, mauris ligula porta nisi, non tristique enim sapien vel nisl. Suspendisse vestibulum lobortis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent nec tempus nibh. Donec mollis commodo metus et fringilla. Etiam venenatis, diam id adipiscing convallis, nisi eros lobortis tellus, feugiat adipiscing ante ante sit amet dolor. Vestibulum vehicula scelerisque facilisis. Sed faucibus placerat bibendum. Maecenas sollicitudin commodo justo, quis hendrerit leo consequat ac. Proin sit amet risus sapien, eget interdum dui. Proin justo sapien, varius sit amet hendrerit id, egestas quis mauris.',
+  }]
 }
 
-function
-var product = document.querySelector('product-box')
-product.innerHTML = '  <span class="on-sale-bubble">Sale!</span>\
-  <div class="product">\
-      <a href="#" class="add-to-card">Add to card</a>\
-      <a href ="product.html">\
-          <img src="../assets/images/products/product-1.jpg">\
-      </a>\
-      <div class="thumb-info">\
-          <a href="#">\
-              <h4> Limited Price</h4>\
-              <h3>$299</h3>\
-          </a>'
+
+// document.addEventListener("DOMContentLoaded", function(event){
+//   document.querySelector('#register-form')
+//   .addEventListener('submit', function(event){
+//     event.preventDefault();
+//     console.log(document.querySelector('#username').value)
+//     console.log(document.querySelector('#password').value)
+//   })
+// });
+
+function getParameterByName(name){
+  var url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
---
+++ Qshop-master/app/products/product.js
@@ -0,0 +1,59 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+    var products = getProductList();
+    //es6 template syntax
+
+
+    var templateProduct = `
+    <h2>__NAME__</h2>
+    <div class="review-number">
+    <span class="review-count">__REVIEWSNUMBER__</span> reviews
+    </div>
+    <p>$__PRICE__</p>
+    <p class="description">__DESCRIPTION__</p>`;
+
+
+
+    var productListNode =  document.querySelector('#product-summary');
+      var div = document.createElement('div');
+      var id = getParameterByName('id')
+      div.className = 'col-sm-12';
+      var productHTML = templateProduct.replace('__NAME__', products[id].name)
+                                       .replace('__PRICE__', products[id].price)
+                                       .replace('__PICTURE__', products[id].picture)
+                                       .replace('__REVIEWSNUMBER__', products[id].reviews.length)
+                                       .replace('__DESCRIPTION__', products[id].description);
+
+      div.innerHTML = productHTML;
+      productListNode.appendChild(div);
+
+      var templateProductContentTab = `
+      <div role="tabpanel" class="tab-pane active" >__DESCRIPTION__</div>
+      <div role="tabpanel" class="tab-pane">
+          <div class="table-responsive table-bordered">
+              <table class="table">
+                  <tbody>
+                  <tr>
+                      <th>Size</th>
+                      <td>20'X 30'</td>
+                  </tr>
+                  <tr>
+                      <th> Colors</th>
+                      <td>Red, Blue</td>
+                  </tr>
+                  <tr>
+                      <th> Material</th>
+                      <td>100% Cotton</td>
+                  </tr>
+                  </tbody>
+              </table>
+          </div>
+      </div>
+      <div role="tabpanel" class="tab-pane" id="reviews">
+         Review here
+      </div>
+      <p>$__PRICE__</p>
+      <p class="description">__DESCRIPTION__</p>`;
+
+
+})
