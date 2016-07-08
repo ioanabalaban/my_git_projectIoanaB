84ef4f9f735a3dabbde71949509f07decbaf6de5
intern2016 <balaban.ioana04@gmail.com>
2016-07-08 09:32:12 +0300

    Utils Update

--- Qshop-master/app/js/utils.js
+++ Qshop-master/app/js/utils.js
@@ -4,7 +4,7 @@
  * @param  {String} inputId element id to check
  */
 function onchangeElem(inputId) {
-  isEmptyInput(inputId) ? showError(inputId) : hideError(inputId);
+    isEmptyInput(inputId) ? showError(inputId) : hideError(inputId);
 }
 
 /**
@@ -13,9 +13,9 @@ function onchangeElem(inputId) {
  * @param  {String} inputId The id of the input we want show the error
  */
 function showError(inputId) {
-  var parentNode =  findParentWithClass(document.querySelector(inputId),'form-group');
-  parentNode.className +=  ' has-error';
-  parentNode.querySelector('.help-block').className = parentNode.querySelector('.help-block').className.replace('hidden','');
+    var parentNode = findParentWithClass(document.querySelector(inputId), 'form-group');
+    parentNode.className += ' has-error';
+    parentNode.querySelector('.help-block').className = parentNode.querySelector('.help-block').className.replace('hidden', '');
 }
 
 /**
@@ -24,11 +24,11 @@ function showError(inputId) {
  * @param  {String} inputId The id of the input we show the error
  */
 function hideError(inputId) {
-  var parentNode =  findParentWithClass(document.querySelector(inputId),'form-group');
-  parentNode.className = parentNode.className.replace('has-error','');
+    var parentNode = findParentWithClass(document.querySelector(inputId), 'form-group');
+    parentNode.className = parentNode.className.replace('has-error', '');
 
-  parentNode.querySelector('.help-block').className = parentNode.querySelector('.help-block').className.replace('hidden','');
-  parentNode.querySelector('.help-block').className +=' hidden';
+    parentNode.querySelector('.help-block').className = parentNode.querySelector('.help-block').className.replace('hidden', '');
+    parentNode.querySelector('.help-block').className += ' hidden';
 }
 
 
@@ -39,20 +39,55 @@ function hideError(inputId) {
  * @return {boolean}
  */
 function isEmptyInput(inputId) {
-  return !document.querySelector(inputId).value
+    return !document.querySelector(inputId).value
 }
 
 
-function findParentWithClass(node, className)
-{
-  if(!node){
-    return null;
-  }
-  if(node.parentNode.className.indexOf(className) !== -1)
-  {
-    return node.parentNode;
-  }
-  else {
-    return findParentWithClass(node.parentNode, className);
-  }
+function findParentWithClass(node, className) {
+    if (!node) {
+        return null;
+    }
+    if (node.parentNode.className.indexOf(className) !== -1) {
+        return node.parentNode;
+    } else {
+        return findParentWithClass(node.parentNode, className);
+    }
 }
+
+function getProductList() {
+    return [{
+        picture: '../assets/images/products/product-1.jpg',
+        name: 'Limited Price',
+        price: 299,
+        onSale: true
+    }, {
+        picture: '../assets/images/products/product-2.jpg',
+        name: 'Amazing Product',
+        price: 299,
+        onSale: false
+    }, {
+        picture: '../assets/images/products/product-3.jpg',
+        name: 'New Product',
+        price: 79,
+        onSale: false
+    } {
+        picture: '../assets/images/products/product-4.jpg',
+        name: 'Limited Product',
+        price: 99,
+        onSale: false
+    }]
+}
+
+function
+var product = document.querySelector('product-box')
+product.innerHTML = '  <span class="on-sale-bubble">Sale!</span>\
+  <div class="product">\
+      <a href="#" class="add-to-card">Add to card</a>\
+      <a href ="product.html">\
+          <img src="../assets/images/products/product-1.jpg">\
+      </a>\
+      <div class="thumb-info">\
+          <a href="#">\
+              <h4> Limited Price</h4>\
+              <h3>$299</h3>\
+          </a>'
